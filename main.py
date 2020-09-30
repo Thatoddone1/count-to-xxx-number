4e688b0a452bb8835a0a6d3b703f094728a4651e
Thatoddone <66497499+Thatoddone1@users.noreply.github.com>
2020-09-30 07:41:43 -0400

    chaned the code to allow it to write the numbers in a file
    it will have error's when faced with lots of nine's but not with like 100000000000000000000000000000 its wired

---
+++ main.py
@@ -0,0 +1,27 @@
+file_builder = open("numberslog.txt", "w+")
+
+whileloop=0
+while whileloop == 0:
+    def count():
+        whilecounting = 1
+        while whatnum+1 > whilecounting:
+            print(whilecounting)
+            file_builder.write(str(whilecounting))
+            file_builder.write('\n')
+            whilecounting=whilecounting+1
+        file_builder.close()
+
+
+    whatnum=int(input('What num to count up to '))
+    shouldstart=input('ok type yes to start(this program will take a while on how big the number is and may lag) ')
+
+    if shouldstart == "yes":
+        print ('ok')
+        count()
+    elif shouldstart == 'no':
+        print ('ok will not start')
+        exit()
+    else:
+        print ("Error: could no process value")
+
+
